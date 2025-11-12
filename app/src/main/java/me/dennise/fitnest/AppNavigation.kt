5f5017539487d75fcb642fb4662f9e4fb4b8f057
Dennise Catolos <dentolos19@gmail.com>
2025-11-12 15:15:58 +0800

    Updated navigation routes

--- app/src/main/java/me/dennise/fitnest/AppNavigation.kt
+++ app/src/main/java/me/dennise/fitnest/AppNavigation.kt
@@ -8,44 +8,50 @@ import me.dennise.fitnest.ui.HomeScreen
 import me.dennise.fitnest.ui.LoginScreen
 import me.dennise.fitnest.ui.RegisterScreen
 
+object AppRoutes {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME = "home"
+}
+
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login") {
-        composable("login") {
+    NavHost(navController = navController, startDestination = AppRoutes.LOGIN) {
+        composable(AppRoutes.LOGIN) {
             LoginScreen(
                 onLoginSuccess = {
                     // Navigate to home/landing screen after successful login
-                    navController.navigate("home") {
-                        popUpTo("login") { inclusive = true }
+                    navController.navigate(AppRoutes.HOME) {
+                        popUpTo(AppRoutes.LOGIN) { inclusive = true }
                     }
                 },
                 onRegisterClick = {
                     // Navigate to register screen
-                    navController.navigate("register")
+                    navController.navigate(AppRoutes.REGISTER)
                 }
             )
         }
 
-        composable("register") {
+        composable(AppRoutes.REGISTER) {
             RegisterScreen(
                 onRegisterSuccess = {
                     // Navigate to home/landing screen after successful registration
-                    navController.navigate("home") {
-                        popUpTo("register") { inclusive = true }
+                    navController.navigate(AppRoutes.HOME) {
+                        popUpTo(AppRoutes.REGISTER) { inclusive = true }
                     }
                 },
                 onCancel = {
                     // Navigate back to login screen when cancel is clicked
-                    navController.navigate("login") {
-                        popUpTo("register") { inclusive = true }
+                    navController.navigate(AppRoutes.LOGIN) {
+                        popUpTo(AppRoutes.REGISTER) { inclusive = true }
                     }
                 }
             )
         }
 
-        composable("home") {
+        composable(AppRoutes.HOME) {
             HomeScreen()
         }
     }
